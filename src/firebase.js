eaeb25fd03c9f83fd601bfc024421de3704772c8
Gabriela Nicolau <gabriella.nicolau@gmail.com>
2021-03-03 11:28:00 -0500

    fixed env variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID
+    apiKey: process.env.NODE_ENV_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NODE_ENV_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NODE_ENV_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NODE_ENV_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NODE_ENV_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NODE_ENV_PUBLIC_APP_ID
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
